cc393af7adec468c943a1141c731d030724ea07a
Jeremy Feng <jeremy.jfeng@gmail.com>
2019-10-22 16:12:45 +1100

    add deploy tool

--- src/apis/jsonPlaceholder.js
+++ src/apis/jsonPlaceholder.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://my-json-server.typicode.com/milk4tea/react-login"
+  baseURL: "https://my-json-server.typicode.com/milk4tea/react-login"
 });
